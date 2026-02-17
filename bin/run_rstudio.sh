0a8979ec2789bf1b4adf9b662a176936f0870193
Dave Tang <davetingpongtang@gmail.com>
2026-02-17 22:38:00 +0900

    R-4.5.2

--- bin/run_rstudio.sh
+++ bin/run_rstudio.sh
@@ -2,25 +2,25 @@
 
 set -euo pipefail
 
-VER=4.4.3
+VER=4.5.2
 IMAGE=davetang/rstudio:${VER}
 NAME=rstudio_server_shiny_${VER}
 PORT=7779
 PACKAGE_DIR=$(pwd)/r_packages_${VER}
 
 if [[ ! -d ${PACKAGE_DIR} ]]; then
-   mkdir ${PACKAGE_DIR}
+   mkdir "${PACKAGE_DIR}"
 fi
 
 docker run \
    --name ${NAME} \
    -d \
    -p ${PORT}:8787 \
-   -v ${PACKAGE_DIR}:/packages \
-   -v $(pwd):/home/rstudio/work \
+   -v "${PACKAGE_DIR}":/packages \
+   -v "$(pwd)":/home/rstudio/work \
    -e PASSWORD=password \
-   -e USERID=$(id -u) \
-   -e GROUPID=$(id -g) \
+   -e USERID="$(id -u)" \
+   -e GROUPID="$(id -g)" \
    ${IMAGE}
 
 >&2 echo ${NAME} listening on port ${PORT}
